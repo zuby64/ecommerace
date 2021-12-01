6585754d003b31dbaa1186b03cd46860202a9bf5
zuby64 <zuby641@gmail.com>
2021-12-02 00:10:52 +0500

    first commit

---
+++ src/data.js
@@ -0,0 +1,64 @@
+export const data  = {
+    products:[
+      {  _id:'1',
+        name:'Nike slim shitrs',
+        category:'Shirts',
+        image:'/images/product.jpeg',
+        price:120,
+        brand:'Nike',
+        rating:4.5,
+        numReviews:10,
+        description:'high quality product'
+    },
+    {  _id:'2',
+    name:'Nike slim shitrs',
+    category:'Shirts',
+    image:'/images/product.jpeg',
+    price:120,
+    brand:'Nike',
+    rating:4.5,
+    numReviews:10,
+    description:'high quality product'
+} ,{  _id:'3',
+name:'Nike slim shitrs',
+category:'Shirts',
+image:'/images/product.jpeg',
+price:120,
+brand:'Nike',
+rating:4.5,
+numReviews:10,
+description:'high quality product'
+},
+{  _id:'4',
+name:'Nike slim shitrs',
+category:'Shirts',
+image:'/images/product.jpeg',
+price:120,
+brand:'Nike',
+rating:4.5,
+numReviews:10,
+description:'high quality product'
+},
+{  _id:'5  ',
+name:'Nike slim shitrs',
+category:'Shirts',
+image:'/images/product.jpeg',
+price:120,
+brand:'Nike',
+rating:4.5,
+numReviews:10,
+description:'high quality product'
+}
+,
+{  _id:'6',
+name:'Nike slim shitrs',
+category:'Shirts',
+image:'/images/product.jpeg',
+price:120,
+brand:'Nike',
+rating:4.5,
+numReviews:10,
+description:'high quality product'
+}
+    ]
+}
